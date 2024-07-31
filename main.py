e7005705d950b359ad8b30038f296df05b5dbe75
Nikolay <me0265826@gmail.com>
2024-07-31 22:41:25 +0300

    Add files via upload
    Lili text editor free editor 
    open source
    version--0.1

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-import numpy as np
-import random
-import scipy as sc
-from matplotlib.colors import LogNorm
-import matplotlib.pyplot as plt
+from tkinter import*
+from tkinter import filedialog
+from tkinter import messagebox
+t=Tk()
+t.title("Lili text editor")
+t.iconphoto(False,PhotoImage(file='image.png'))
+t["bg"]="#000033"
+f_text=Frame(t)
+f_text['bg']='#000033'
+text=Text(f_text,bg="#000033",fg="lime",wrap=WORD,insertbackground="lime",selectbackground="#000099",spacing3=10)
+def save(event):
+    filepath = filedialog.asksaveasfilename(title="Save file", initialdir="C:/", defaultextension="txt", initialfile="lilli.txt")
+    if filepath != "":
+        text1 = text.get("1.0", END)
+        with open(filepath, "w") as file:
+            file.write(text1)
 
-fig, axs = plt.subplots(1, 1, layout='constrained')
-X, Y = np.meshgrid(np.linspace(-3, 5, 100), np.linspace(-3, 5, 100))
-j=2.
-j1=3.
-Z = np.sinc(X) * np.expm1(Y)-np.arccosh(X)//np.matmul(Y,X)+np.fft.fft(Y)/np.transpose(Y)+Y+np.random.standard_normal(100)+np.arcsinh(Y)//np.mean(Y)+np.log10(X)+Y.cumsum(axis=0)+X.cumsum(axis=0)+np.flipud(X)
-co = axs.contourf(X, Y, Z, levels=np.linspace(-1.25, 2, 2))
+def openfile(event):
+    filepath1=filedialog.askopenfilename(title="Open file", initialdir="C:/", defaultextension="lili.txt", initialfile="")
+    if filepath1 != "":
+        with open(filepath1, "r") as file:
+            text1 = file.read()
+            text.insert("1.0", text1)
+def help(event):
+    messagebox.askokcancel("help Lili editor","up-save down-open")
+def version(event):
+    messagebox.showwarning("version Lili editor", "0.1 release")
 
-plt.show()
+f_text.pack(fill=BOTH,expand=1)
+scroll=Scrollbar(f_text,command=text.yview)
+scroll.pack(side=RIGHT,fill=Y)
+text.pack(expand=1,fill=BOTH,side=LEFT,padx=10,pady=10)
+text.bind("<KeyPress-Up>",save)
+text.bind("<KeyPress-Down>",openfile)
+text.bind("<KeyPress-Left>",help)
+text.bind("<KeyPress-Right>",version)
+text.config(yscrollcommand=scroll.set)
+
+t.mainloop()
